76594c766c6ea9f0269719d476d15116eeed6374
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-03 16:54:24 +0900

    Style: Refactor Code Style of DefaultDialogFragment

--- app/src/main/java/com/pleiades/pleione/slotgallery/ui/fragment/dialog/DefaultDialogFragment.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/ui/fragment/dialog/DefaultDialogFragment.kt
@@ -1,19 +1,14 @@
 package com.pleiades.pleione.slotgallery.ui.fragment.dialog
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.Dialog
 import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.FragmentActivity
+import androidx.core.view.isVisible
 import com.pleiades.pleione.slotgallery.Config.Companion.DIALOG_TYPE_PERMISSION
 import com.pleiades.pleione.slotgallery.Config.Companion.DIALOG_WIDTH_PERCENTAGE_DEFAULT
 import com.pleiades.pleione.slotgallery.Config.Companion.PERMISSION_IMAGES_VIDEOS
@@ -23,86 +18,76 @@ import com.pleiades.pleione.slotgallery.Config.Companion.REQUEST_CODE_PERMISSION
 import com.pleiades.pleione.slotgallery.R
 import com.pleiades.pleione.slotgallery.controller.DeviceController
 import com.pleiades.pleione.slotgallery.databinding.FragmentDialogDefaultBinding
-import com.pleiades.pleione.slotgallery.databinding.FragmentMainBinding
 
 class DefaultDialogFragment(private val type: Int) : androidx.fragment.app.DialogFragment() {
     private var _binding: FragmentDialogDefaultBinding? = null
     private val binding get() = _binding!!
 
-    @SuppressLint("InflateParams", "UseRequireInsteadOfGet")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // initialize binding
+        // binding
         _binding = FragmentDialogDefaultBinding.inflate(requireActivity().layoutInflater)
 
-        // initialize builder
-        val builder = context?.let { AlertDialog.Builder(it) }
-
-        // initialize activity
-        val activity: FragmentActivity? = activity
-        if (activity != null) {
-            // initialize and set message
-            val messageTextView = binding.messageDialogDefault
-            when (type) {
-                DIALOG_TYPE_PERMISSION -> messageTextView.setText(R.string.dialog_message_permission)
-            }
-
-            // set positive listener
-            binding.positiveDialogDefault.setOnClickListener {
+        // message
+        binding.message.text =
+            getString(
                 when (type) {
-                    DIALOG_TYPE_PERMISSION -> {
-                        if (Build.VERSION.SDK_INT >= 33)
-                            (context as Activity).requestPermissions(PERMISSION_IMAGES_VIDEOS, REQUEST_CODE_PERMISSION_IMAGES_VIDEOS)
-                        else
-                            (context as Activity).requestPermissions(PERMISSION_STORAGE, REQUEST_CODE_PERMISSION_STORAGE)
-                    }
+                    DIALOG_TYPE_PERMISSION -> R.string.message_dialog_permission
+                    else -> R.string.message_invalid
                 }
-                dismiss()
-            }
+            )
 
-            // set negative listener
-            binding.negativeDialogDefault.setOnClickListener {
-                dismiss()
-            }
-
-            // set negative visibility
-            val negativeTextView = binding.negativeDialogDefault
+        // positive button
+        binding.positiveButton.setOnClickListener {
             when (type) {
-                DIALOG_TYPE_PERMISSION -> negativeTextView.visibility = View.INVISIBLE
-                else -> negativeTextView.visibility = View.VISIBLE
+                DIALOG_TYPE_PERMISSION -> {
+                    if (Build.VERSION.SDK_INT >= 33)
+                        requireActivity().requestPermissions(PERMISSION_IMAGES_VIDEOS, REQUEST_CODE_PERMISSION_IMAGES_VIDEOS)
+                    else
+                        requireActivity().requestPermissions(PERMISSION_STORAGE, REQUEST_CODE_PERMISSION_STORAGE)
+                }
             }
-
-            // set dialog view
-            builder!!.setView(binding.root)
+            dismiss()
         }
 
-        // create dialog
-        val dialog: AlertDialog = builder!!.create()
-
-        // set transparent background
-        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        // set canceled on touch outside
-        when (type) {
-            DIALOG_TYPE_PERMISSION -> dialog.setCanceledOnTouchOutside(false)
-            else -> dialog.setCanceledOnTouchOutside(true)
+        // negative button
+        binding.negativeButton.run {
+            isVisible = when (type) {
+                DIALOG_TYPE_PERMISSION -> false
+                else -> true
+            }
+            setOnClickListener {
+                dismiss()
+            }
         }
-        return dialog
+
+        return AlertDialog
+            .Builder(requireContext())
+            .apply { setView(binding.root) }
+            .create()
+            .apply {
+                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                setCanceledOnTouchOutside(
+                    when (type) {
+                        DIALOG_TYPE_PERMISSION -> false
+                        else -> true
+                    }
+                )
+                setCancelable(
+                    when (type) {
+                        DIALOG_TYPE_PERMISSION -> false
+                        else -> true
+                    }
+                )
+            }
     }
 
     override fun onStart() {
         super.onStart()
 
-        val width = (DeviceController.getWidthMax(requireContext()) * DIALOG_WIDTH_PERCENTAGE_DEFAULT).toInt()
-        val height = ViewGroup.LayoutParams.WRAP_CONTENT
-        dialog!!.window!!.setLayout(width, height)
-
-        // set cancelable
-        when (type) {
-            DIALOG_TYPE_PERMISSION -> dialog!!.setCancelable(false)
-            else -> {
-                dialog!!.setCancelable(true)
-            }
-        }
+        dialog?.window?.setLayout(
+            /* width = */ (DeviceController.getWidthMax(requireContext()) * DIALOG_WIDTH_PERCENTAGE_DEFAULT).toInt(),
+            /* height = */ ViewGroup.LayoutParams.WRAP_CONTENT
+        )
     }
 
     override fun onCancel(dialog: DialogInterface) {
