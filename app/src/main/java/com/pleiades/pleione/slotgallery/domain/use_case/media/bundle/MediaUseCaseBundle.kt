c814e1139cbbf0d5c9fd26b4ac129b1db5396bc2
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-06 14:44:36 +0900

    Add: MediaUseCaseBundle

---
+++ app/src/main/java/com/pleiades/pleione/slotgallery/domain/use_case/media/bundle/MediaUseCaseBundle.kt
@@ -0,0 +1,12 @@
+package com.pleiades.pleione.slotgallery.domain.use_case.media.bundle
+
+import com.pleiades.pleione.slotgallery.domain.use_case.media.CopyDirectoryUseCase
+import com.pleiades.pleione.slotgallery.domain.use_case.media.CopyMediaUseCase
+import com.pleiades.pleione.slotgallery.domain.use_case.media.GetDirectoryListUseCase
+import com.pleiades.pleione.slotgallery.domain.use_case.slot.*
+
+data class MediaUseCaseBundle(
+    val getDirectoryListUseCase: GetDirectoryListUseCase,
+    val copyDirectoryUseCase: CopyDirectoryUseCase,
+    val copyMediaUseCase: CopyMediaUseCase
+)
