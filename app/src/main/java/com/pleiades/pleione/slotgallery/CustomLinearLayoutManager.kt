6555647ce8b0ec66dc5220e68b7b696ad99bdc9a
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-06 10:35:47 +0900

    Refactor: Remove NonScrollLinearLayoutManager

--- app/src/main/java/com/pleiades/pleione/slotgallery/CustomLinearLayoutManager.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/CustomLinearLayoutManager.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class NonScrollLinearLayoutManager(var context: Context) : LinearLayoutManager(context) {
-    override fun canScrollVertically(): Boolean {
-        return false
-    }
+//    override fun canScrollVertically(): Boolean {
+//        return false
+//    }
 }
