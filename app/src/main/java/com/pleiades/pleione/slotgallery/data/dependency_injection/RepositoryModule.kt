d457024e10834dc0df805884cf305e956131410a
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-06 16:42:00 +0900

    Feat: Hilt Repository Module

---
+++ app/src/main/java/com/pleiades/pleione/slotgallery/data/dependency_injection/RepositoryModule.kt
@@ -0,0 +1,35 @@
+package com.pleiades.pleione.slotgallery.data.dependency_injection
+
+import com.pleiades.pleione.slotgallery.data.repository.DefaultMediaRepository
+import com.pleiades.pleione.slotgallery.data.repository.DefaultSlotRepository
+import com.pleiades.pleione.slotgallery.data.repository.DefaultWindowRepository
+import com.pleiades.pleione.slotgallery.domain.repository.MediaRepository
+import com.pleiades.pleione.slotgallery.domain.repository.SlotRepository
+import com.pleiades.pleione.slotgallery.domain.repository.WindowRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMediaRepository(
+        defaultMediaRepository: DefaultMediaRepository
+    ): MediaRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindSlotRepository(
+        defaultSlotRepository: DefaultSlotRepository
+    ): SlotRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindWindowRepository(
+        defaultWindowRepository: DefaultWindowRepository
+    ): WindowRepository
+}
