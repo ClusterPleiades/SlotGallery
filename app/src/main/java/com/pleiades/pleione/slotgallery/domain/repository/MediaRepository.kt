87c87420d1c55a2ca795fb09dd1164b702d7801b
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-06 14:39:39 +0900

    Add: MediaRepository

---
+++ app/src/main/java/com/pleiades/pleione/slotgallery/domain/repository/MediaRepository.kt
@@ -0,0 +1,19 @@
+package com.pleiades.pleione.slotgallery.domain.repository
+
+import com.pleiades.pleione.slotgallery.domain.model.Content
+import com.pleiades.pleione.slotgallery.domain.model.Directory
+
+interface MediaRepository {
+    fun getDirectoryList(): List<Directory>
+
+    suspend fun copyDirectory(
+        fromDirectorySet: Set<Directory>,
+        toDirectory: Directory
+    )
+
+    suspend fun copyMedia(
+        fromDirectory: Directory,
+        toDirectory: Directory,
+        mediaSet: Set<Content>
+    )
+}
