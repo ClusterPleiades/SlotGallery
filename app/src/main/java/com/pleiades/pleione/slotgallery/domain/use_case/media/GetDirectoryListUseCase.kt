a9fb60fd2f584547a14fdb3481500e012defd1f5
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-07 10:40:10 +0900

    Modify: GetDirectoryListUseCase에 Parameter 정의

--- app/src/main/java/com/pleiades/pleione/slotgallery/domain/use_case/media/GetDirectoryListUseCase.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/domain/use_case/media/GetDirectoryListUseCase.kt
@@ -1,7 +1,8 @@
 package com.pleiades.pleione.slotgallery.domain.use_case.media
 
+import com.pleiades.pleione.slotgallery.domain.model.Slot
 import com.pleiades.pleione.slotgallery.domain.repository.MediaRepository
 
 class GetDirectoryListUseCase(private val repository: MediaRepository) {
-    operator fun invoke() = repository.getDirectoryList()
+    operator fun invoke(selectedSlot: Slot) = repository.getDirectoryList(selectedSlot)
 }
