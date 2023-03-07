e6c5860c7447fd3d5bed51d82f7531eb17ff785f
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-07 15:13:46 +0900

    Modify: Directory를 data 클래스로 변경

--- app/src/main/java/com/pleiades/pleione/slotgallery/domain/model/Directory.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/domain/model/Directory.kt
@@ -2,30 +2,9 @@ package com.pleiades.pleione.slotgallery.domain.model
 
 import com.pleiades.pleione.slotgallery.Config.Companion.PATH_PRIMARY
 
-class Directory(val directoryOverview: DirectoryOverview) {
+data class Directory(val directoryOverview: DirectoryOverview) {
     val name = directoryOverview.lastPath.substringAfter(PATH_PRIMARY).substringAfterLast("/")
-    val mediaArrayList: ArrayList<Media> = ArrayList()
-    var date = 0L
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null) return false
-        if (javaClass != other.javaClass) return false
-
-        val otherDirectory = other as Directory
-        return directoryOverview == otherDirectory.directoryOverview
-                && date == otherDirectory.date
-                && mediaArrayList == otherDirectory.mediaArrayList
-    }
-
-    override fun hashCode(): Int {
-        var result = directoryOverview.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + mediaArrayList.hashCode()
-        result = 31 * result + date.hashCode()
-        return result
-    }
-
-    fun refreshDate() {
-        date = mediaArrayList.maxOf { it.date }
-    }
+    val mediaMutableList: MutableList<Media> = mutableListOf()
+    val date: Long
+        get() = mediaMutableList.maxOf { it.date }
 }
