5f07abf12c5292fbe66cccf909aa4555254e0e53
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-07 21:05:35 +0900

    Feat: Setting State, ViewModel 구현

--- app/src/main/java/com/pleiades/pleione/slotgallery/presentation/setting/SettingViewModel.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/presentation/setting/SettingViewModel.kt
@@ -1,11 +1,9 @@
 package com.pleiades.pleione.slotgallery.presentation.setting
 
 import androidx.lifecycle.ViewModel
-import com.bumptech.glide.Glide.init
-import com.pleiades.pleione.slotgallery.domain.use_case.media.bundle.MediaUseCaseBundle
+import com.pleiades.pleione.slotgallery.domain.model.DirectoryOverview
+import com.pleiades.pleione.slotgallery.domain.model.Slot
 import com.pleiades.pleione.slotgallery.domain.use_case.slot.bundle.SlotUseCaseBundle
-import com.pleiades.pleione.slotgallery.domain.use_case.window.GetWidthUseCase
-import com.pleiades.pleione.slotgallery.presentation.main.MainState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,5 +13,89 @@ import javax.inject.Inject
 class SettingViewModel @Inject constructor(
     private val slotUseCaseBundle: SlotUseCaseBundle
 ) : ViewModel() {
-    fun getSelectedSlot() = slotUseCaseBundle.getSelectedSlotUseCase()
+    private val _state = MutableStateFlow(SettingState())
+    val state = _state.asStateFlow()
+
+    init {
+        _state.value = state.value.copy(
+            selectedSlotPosition = slotUseCaseBundle.getSelectedSlotPositionUseCase(),
+            slotList = slotUseCaseBundle.getSlotListUseCase()
+        )
+    }
+
+    fun getSelectedSlot(): Slot? {
+        val slotList = state.value.slotList
+        val selectedSlotPosition = state.value.selectedSlotPosition
+
+        return if (slotList.isEmpty()) null
+        else slotList[selectedSlotPosition]
+    }
+
+    fun addDirectoryOverView(directoryOverview: DirectoryOverview) {
+        getSelectedSlot()?.let {
+            it.directoryOverviewMutableList.add(directoryOverview)
+            _state.value = state.value.copy()
+        }
+    }
+
+    fun removeDirectoryOverView(position: Int) {
+        getSelectedSlot()?.let {
+            it.directoryOverviewMutableList.removeAt(position)
+            _state.value = state.value.copy()
+        }
+    }
+
+    fun toggleDirectoryOverViewVisibility(position: Int) {
+        getSelectedSlot()?.let {
+            with(it.directoryOverviewMutableList[position]) { isVisible = !isVisible }
+            _state.value = state.value.copy()
+        }
+    }
+
+    fun selectSlot(position: Int) {
+        slotUseCaseBundle.putSelectedSlotPositionUseCase(position)
+        _state.value = state.value.copy(
+            selectedSlotPosition = position
+        )
+    }
+
+    fun addSlot(name: String) {
+        val slotList = mutableListOf<Slot>().apply {
+            addAll(state.value.slotList)
+            add(Slot(name))
+        }
+        slotUseCaseBundle.putSlotListUseCase(slotList)
+        _state.value = state.value.copy(
+            slotList = slotList
+        )
+    }
+
+    fun removeSlot(position: Int) {
+        val slotList = mutableListOf<Slot>().apply {
+            addAll(state.value.slotList)
+        }
+        slotList.removeAt(position)
+        slotUseCaseBundle.putSlotListUseCase(slotList)
+
+        var selectedSlotPosition = state.value.selectedSlotPosition
+        if ((position < selectedSlotPosition) || (position == selectedSlotPosition && position > 0)) selectedSlotPosition--
+        slotUseCaseBundle.putSelectedSlotPositionUseCase(selectedSlotPosition)
+
+        _state.value = state.value.copy(
+            selectedSlotPosition = selectedSlotPosition,
+            slotList = slotList
+        )
+    }
+
+    fun renameSlot(position: Int, name: String) {
+        val slotList = mutableListOf<Slot>().apply {
+            addAll(state.value.slotList)
+        }.also {
+            it[position].name = name
+        }
+        slotUseCaseBundle.putSlotListUseCase(slotList)
+        _state.value = state.value.copy(
+            slotList = slotList
+        )
+    }
 }
