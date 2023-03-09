4090ea47b40a397f4bfc5dc9a4959f54c40a76b5
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-09 20:18:50 +0900

    Refactor: ProgressDialogFragment, State 리팩토링

---
+++ app/src/main/java/com/pleiades/pleione/slotgallery/presentation/dialog/progress/ProgressDialogState.kt
@@ -0,0 +1,10 @@
+package com.pleiades.pleione.slotgallery.presentation.dialog.progress
+
+import kotlinx.coroutines.Job
+
+data class ProgressDialogState(
+    val isCanceled: Boolean = false,
+    val progress: Int = 0,
+    val maxProgress: Int = 0,
+    val job: Job? = null
+)
