4cf9a627620a709065ecf9f5777d519cbd90e304
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-10 11:03:01 +0900

    Style: needless blank line 제거

--- app/src/test/java/com/pleiades/pleione/slotgallery/ExampleUnitTest.kt
+++ app/src/test/java/com/pleiades/pleione/slotgallery/ExampleUnitTest.kt
@@ -3,7 +3,6 @@ package com.pleiades.pleione.slotgallery
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
