50eb1a7c92f21fe6b7813c28a64e6c6fbf77cc62
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-10 18:32:56 +0900

    Modify: DirectoryOverview를 Parcelable 데이터로 변경

--- app/src/main/java/com/pleiades/pleione/slotgallery/domain/model/Directory.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/domain/model/Directory.kt
@@ -1,9 +1,7 @@
 package com.pleiades.pleione.slotgallery.domain.model
 
-import com.pleiades.pleione.slotgallery.Config.Companion.PATH_PRIMARY
-
 data class Directory(val directoryOverview: DirectoryOverview) {
-    val name = directoryOverview.lastPath.substringAfter(PATH_PRIMARY).substringAfterLast("/")
+    val name = directoryOverview.toString()
     val mediaMutableList: MutableList<Media> = mutableListOf()
     val date: Long
         get() = mediaMutableList.maxOf { it.date }
--- app/src/main/java/com/pleiades/pleione/slotgallery/domain/model/DirectoryOverview.kt
+++ app/src/main/java/com/pleiades/pleione/slotgallery/domain/model/DirectoryOverview.kt
@@ -1,9 +1,15 @@
 package com.pleiades.pleione.slotgallery.domain.model
 
+import android.os.Parcelable
+import com.pleiades.pleione.slotgallery.Config
 import com.pleiades.pleione.slotgallery.Config.Companion.URI_DEFAULT_DIRECTORY
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class DirectoryOverview(
     val uri: String = URI_DEFAULT_DIRECTORY,
     val lastPath: String,
     var isVisible: Boolean = true
-)
+) : Parcelable {
+    override fun toString(): String = lastPath.substringAfter(Config.PATH_PRIMARY).substringAfterLast("/")
+}
