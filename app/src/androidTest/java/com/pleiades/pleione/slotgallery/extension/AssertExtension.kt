b98dea06f061060c6a6609d9ea50f66a6206b25b
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-03-10 13:46:57 +0900

    Modify: testAssertEquals 파라미터 타입 변경

--- app/src/androidTest/java/com/pleiades/pleione/slotgallery/extension/AssertExtension.kt
+++ app/src/androidTest/java/com/pleiades/pleione/slotgallery/extension/AssertExtension.kt
@@ -2,6 +2,6 @@ package com.pleiades.pleione.slotgallery.extension
 
 import org.junit.Assert
 
-fun testAssertEquals(expected: Number, actual: Number) {
+fun testAssertEquals(expected: Any, actual: Any) {
     Assert.assertEquals(expected, actual)
 }
