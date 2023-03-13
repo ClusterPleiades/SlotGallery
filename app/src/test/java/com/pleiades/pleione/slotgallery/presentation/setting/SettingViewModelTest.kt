bd029265b060dc0e74cb75efcb01ed98c0b9debc
kit(김광수) <kit.t@kakaocorp.com>
2023-03-13 10:19:07 +0900

    Test: SettingViewModelTest 추가

---
+++ app/src/test/java/com/pleiades/pleione/slotgallery/presentation/setting/SettingViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.pleiades.pleione.slotgallery.presentation.setting
+
+import org.junit.Test
+
+class SettingViewModelTest {
+    @Test
+    fun test() {
+        TODO("구현해야 함")
+    }
+}
