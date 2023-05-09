ff47564c3b00675a4ba424f4932d07c8f16a0120
jjoo.s <Pleione.P.Cluster@gmail.com>
2023-05-09 18:34:09 +0900

    Rename: Media to Page

---
+++ app/src/main/java/com/pleiades/pleione/slotgallery/presentation/main/pager/page/PageFragment.kt
@@ -0,0 +1,83 @@
+package com.pleiades.pleione.slotgallery.presentation.main.pager.page
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.pleiades.pleione.slotgallery.Config.Companion.INTENT_EXTRA_NAME
+import com.pleiades.pleione.slotgallery.Config.Companion.INTENT_EXTRA_URI
+import com.pleiades.pleione.slotgallery.R
+import com.pleiades.pleione.slotgallery.databinding.FragmentPageBinding
+import com.pleiades.pleione.slotgallery.presentation.main.MainActivity
+import com.pleiades.pleione.slotgallery.ui.media.video.VideoActivity
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
+
+@AndroidEntryPoint
+class PageFragment : Fragment() {
+    private var _binding: FragmentPageBinding? = null
+    private val binding get() = _binding!!
+    private val fragmentViewModel: PageViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // initialize views
+//        binding.photoImage.setOnClickListener { (activity as MediaActivity).fullImage() }
+
+        // image
+        Glide.with(requireContext())
+            .load(fragmentViewModel.media.uri)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.image)
+
+        // video
+        if (fragmentViewModel.media.isVideo) {
+            binding.play.visibility = View.VISIBLE
+            binding.time.visibility = View.VISIBLE
+
+            val minutes = TimeUnit.MILLISECONDS.toMinutes(fragmentViewModel.media.duration)
+            val seconds = TimeUnit.MILLISECONDS.toSeconds(fragmentViewModel.media.duration) % 60
+            val time = String.format("%02d:%02d", minutes, seconds)
+            binding.time.text = time
+
+            binding.image.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.color_transparent_black
+                )
+            )
+
+            binding.play.setOnClickListener {
+                val intent = Intent(context, VideoActivity::class.java)
+                intent.putExtra(INTENT_EXTRA_NAME, fragmentViewModel.media.name)
+                intent.putExtra(INTENT_EXTRA_URI, fragmentViewModel.media.uri.toString())
+                startActivity(intent)
+            }
+        }
+    }
+}
